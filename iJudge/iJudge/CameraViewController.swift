b2b3fd885290972f420f98eed9ccbf2b6e28722e
Neal Goyal <ngoya001@ucr.edu>
2018-04-22 11:58:39 -0700

    Added Face API detection

--- iJudge/iJudge/CameraViewController.swift
+++ iJudge/iJudge/CameraViewController.swift
@@ -20,7 +20,7 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
 
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var button: UIButton!
-    
+
     var captureSession = AVCaptureSession()
     var sessionOutput = AVCapturePhotoOutput()
     var previewLayer = AVCaptureVideoPreviewLayer()
@@ -28,6 +28,11 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     var usersStorageRef: StorageReference!
     var personImage: UIImage!
     var photoType: PhotoType!
+    
+    var actIdc: UIActivityIndicatorView!
+    
+    var faceFromPhoto: MPOFace!
+    var faceFromFirebase: MPOFace!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -88,6 +93,7 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
             let userID = Auth.auth().currentUser?.uid
             let imageRef = usersStorageRef.child("\(userID!).jpg")
             
+            showActivityIndicator(onView: self.view)
             if photoType == PhotoType.signup{
                 self.personImage = UIImage(data: dataImage)
                 
@@ -102,12 +108,13 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
                         return
                     }
                     
-                    if (faces!.count) > 1 || faces == nil {
+                    if (faces!.count) > 1 || (faces!.count) == 0 {
                         print("too many or not at all faces")
+                        self.failLogin()
                         return
                     }
                     
-                    let uploadTask = imageRef.put(dataImage, metadata: nil, completion: { (metadata, error) in
+                    let uploadTask = imageRef.putData(dataImage, metadata: nil, completion: { (metadata, error) in
                         if error != nil {
                             print(error!)
                             return
@@ -126,9 +133,129 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
                 
             }else if photoType == PhotoType.login {
                 //verify the user
+                self.personImage = UIImage(data: dataImage)
+                
+                captureSession.stopRunning()
+                
+                imageRef.downloadURL(completion: { (url, error) in
+                    
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    
+                    
+                    self.verify(withURL: url!.absoluteString)
+                    
+                })
             }
             
         }
+    }
+    
+    func verify(withURL url: String) {
+        
+        let client = MPOFaceServiceClient(subscriptionKey: "e8cd67e49d78462c83d242e9495bca21")!
+        
+        let data = UIImageJPEGRepresentation(self.personImage!, 0.8)
+        
+        
+        client.detect(with: data, returnFaceId: true, returnFaceLandmarks: true, returnFaceAttributes: [], completionBlock: { (faces, error) in
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            if (faces!.count) > 1 || (faces!.count) == 0 {
+                print("too many or not at all faces")
+                self.failLogin()
+                self.actIdc.stopAnimating()
+                return
+            }
+            
+            self.faceFromPhoto = faces![0]
+            
+            
+            client.detect(withUrl: url, returnFaceId: true, returnFaceLandmarks: true, returnFaceAttributes: [], completionBlock: { (faces, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                self.faceFromFirebase = faces![0]
+                
+                client.verify(withFirstFaceId: self.faceFromPhoto.faceId, faceId2: self.faceFromFirebase.faceId, completionBlock: { (result, error) in
+                    
+                    
+                    if error != nil{
+                        print(error!)
+                        return
+                    }
+                    
+                    self.actIdc.stopAnimating()
+                    if result!.isIdentical {
+                        // THE PERSON IS THE SAME
+                        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Logged")
+                        self.present(vc, animated: true, completion: nil)
+                    }else {
+                        self.failLogin()
+                    }
+                    
+                    
+                    
+                })
+                
+            })
+            
+            
+            
+        })
+        
+    }
+    
+    func showActivityIndicator(onView: UIView) {
+        let container: UIView = UIView()
+        container.frame = onView.frame
+        container.center = onView.center
+        container.backgroundColor = UIColor(white: 0, alpha: 0.8)
+        
+        let loadingView: UIView = UIView()
+        loadingView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        loadingView.center = onView.center
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        
+        
+        
+        actIdc = UIActivityIndicatorView()
+        actIdc.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        actIdc.hidesWhenStopped = true
+        actIdc.activityIndicatorViewStyle = .whiteLarge
+        actIdc.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2)
+        
+        loadingView.addSubview(actIdc)
+        container.addSubview(loadingView)
+        onView.addSubview(container)
+        
+        actIdc.startAnimating()
+    }
+    
+    func failLogin() {
+        // THE PERSON IS NOT THE SAME
+        let alert = UIAlertController(title: "Failed Login", message: "Not same person", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            let logvc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+            self.present(logvc, animated: true, completion: nil)
+        })
+        
+        alert.addAction(action)
+        do {
+            try Auth.auth().signOut()
+        }catch{
+        }
+        self.present(alert, animated: true, completion: nil)
+        
     }
 
     @IBAction func takePhoto(_ sender: Any) {
--- iJudge/iJudge/LoginViewController.swift
+++ iJudge/iJudge/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
@@ -21,6 +22,21 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginPressed(_ sender: Any) {
+        guard emailField.text != "", pwField.text != "" else {return}
+        
+        
+        Auth.auth().signIn(withEmail: emailField.text!, password: pwField.text!, completion: { (user, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            let cameraVC = UIStoryboard(name: "Camera", bundle: nil).instantiateInitialViewController() as! CameraViewController
+            
+            cameraVC.photoType = PhotoType.login
+            self.present(cameraVC, animated: true, completion: nil)
+            
+        })
     }
     
 
